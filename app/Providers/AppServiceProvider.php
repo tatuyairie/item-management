1352a2b2fe538fb90165ebea87a47fc8277e763c
tatuyairie <tatuya.irie@gmail.com>
2022-12-09 12:41:28 +0900

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(\App::environment(['production'])){
-            \URL::forceScheme('https');
+        // if(\App::environment(['production'])){
+        //     \URL::forceScheme('https');
     }
 }
