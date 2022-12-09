2c8b07f5fae7cec3a2cb869dc365e061a3bbea08
tatuyairie <tatuya.irie@gmail.com>
2022-12-09 12:57:46 +0900

    デザイン修正

--- resources/views/sales/list.blade.php
+++ resources/views/sales/list.blade.php
@@ -23,9 +23,9 @@
                 <div class="card-header">
                     <h3 class="card-title">売上一覧</h3>
                 </div>
-                <div class="card-body table-responsive p-0">
+                <div class="card-body table-responsive p-0" style="height: 75vh">
                     <table class="table table-hover text-nowrap">
-                        <thead>
+                        <thead class="sticky-top bg-white">
                             <tr>
                                 <th class="sort" data-sort="id" width="50px">商品コード</th>
                                 <th class="sort" data-sort="name">商品名</th>
